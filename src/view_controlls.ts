3bb14fdfe712047cc7ef50c40bb98fb56ea11a21
Grzegorz Maniak <git@grzegorz.ie>
2023-06-25 19:38:32 +0100

    Refactored stolen code

--- src/view_controlls.ts
+++ src/view_controlls.ts
@@ -2,28 +2,25 @@ import konva, {} from 'konva';
 
 const scaleBy = 1.01;
 
-function getDistance(p1, p2) {
-  return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-}
+const is_touch_enabled = () => ('ontouchstart' in window) || 
+(navigator.maxTouchPoints > 0) || (navigator.maxTouchPoints > 0);
 
-function getCenter(p1, p2) {
-  return {
+const get_center = (p1, p2) => ({
     x: (p1.x + p2.x) / 2,
     y: (p1.y + p2.y) / 2,
-  };
-}
+});
 
-function isTouchEnabled() { 
-  return ( 'ontouchstart' in window ) ||  
-         ( navigator.maxTouchPoints > 0 ) ||  
-         ( navigator.maxTouchPoints > 0 ); 
-} 
+const get_distance = (p1, p2) => Math.sqrt(
+    Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)
+);
 
 export function handle_controlls(stage: konva.Stage) {
-    let lastCenter = null;
-    let lastDist = 0;
+    let last_center = null;
+    let last_dist = 0;
         
-    function zoomStage(event) {
+
+
+    const zoom_stage = (event) => {
         event.evt.preventDefault();
 
         const oldScale = stage.scaleX();
@@ -43,79 +40,72 @@ export function handle_controlls(stage: konva.Stage) {
 
         stage.position(newPos);
         stage.batchDraw();
-        }
-
-    function handleTouch(e) {
-        e.evt.preventDefault();
-        var touch1 = e.evt.touches[0];
-        var touch2 = e.evt.touches[1];
-
-        if (touch1 && touch2) {
-            if (stage.isDragging()) {
-                stage.stopDrag();
-            }
-
-            var p1 = {
-                x: touch1.clientX,
-                y: touch1.clientY
-            };
-
-            var p2 = {
-                x: touch2.clientX,
-                y: touch2.clientY
-            };
-
-            if (!lastCenter) {
-                lastCenter = getCenter(p1, p2);
-                return;
-            }
-
-            var newCenter = getCenter(p1, p2);
-
-            var dist = getDistance(p1, p2);
-
-            if (!lastDist) {
-                lastDist = dist;
-            }
-
-            // local coordinates of center point
-            var pointTo = {
-                x: (newCenter.x - stage.x()) / stage.scaleX(),
-                y: (newCenter.y - stage.y()) / stage.scaleX()
-            };
-
-            var scale = stage.scaleX() * (dist / lastDist);
-
-            stage.scaleX(scale);
-            stage.scaleY(scale);
-
-            // calculate new position of the stage
-            var dx = newCenter.x - lastCenter.x;
-            var dy = newCenter.y - lastCenter.y;
-
-            var newPos = {
-                x: newCenter.x - pointTo.x * scale + dx,
-                y: newCenter.y - pointTo.y * scale + dy
-            };
-
-            stage.position(newPos);
-            stage.batchDraw();
-
-            lastDist = dist;
-            lastCenter = newCenter;
-        }
     }
 
-    function handleTouchEnd() {
-        lastCenter = null;
-        lastDist = 0;
+
+
+    const handle_touch = (e) => {
+        e.evt.preventDefault();
+
+        let t1 = e.evt.touches[0],
+            t2 = e.evt.touches[1];
+        if (!t1 || !t2) return;
+
+
+        if (stage.isDragging())
+            stage.stopDrag();
+            
+        
+        let p1 = { x: t1.clientX, y: t1.clientY },
+            p2 = { x: t2.clientX, y: t2.clientY };
+
+        if (!last_center) return last_center = get_center(p1, p2);
+    
+
+        let new_center = get_center(p1, p2),
+            dist = get_distance(p1, p2);
+
+        if (!last_dist) last_dist = dist;
+        
+
+        // -- local coordinates of center point
+        const point_to = {
+            x: (new_center.x - stage.x()) / stage.scaleX(),
+            y: (new_center.y - stage.y()) / stage.scaleX()
+        };
+
+        const scale = stage.scaleX() * (dist / last_dist);
+        stage.scaleX(scale);
+        stage.scaleY(scale);
+
+
+        // -- calculate new position of the stage
+        const dx = new_center.x - last_center.x,
+            dy = new_center.y - last_center.y;
+
+        const new_pos = {
+            x: new_center.x - point_to.x * scale + dx,
+            y: new_center.y - point_to.y * scale + dy
+        };
+
+
+        // -- Set changes and draw 
+        stage.position(new_pos);
+        stage.batchDraw();
+        last_dist = dist;
+        last_center = new_center;
+    }
+
+    const touch_end = () => {
+        last_center = null;
+        last_dist = 0;
     }
 
 
     // -- Event listeners --
-    stage.draggable(!isTouchEnabled());
-    stage.on('wheel', zoomStage);
-    stage.on('touchstart', handleTouch);
-    stage.on('touchmove', handleTouch);
-    stage.on('touchend', handleTouchEnd);
+    stage.draggable(!is_touch_enabled());
+    stage.on('wheel', zoom_stage);
+    stage.on('touchstart', handle_touch);
+    stage.on('touchmove', handle_touch);
+    stage.on('touchend', touch_end);
 }

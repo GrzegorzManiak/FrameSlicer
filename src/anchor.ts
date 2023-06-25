0903ebdc215ae4daca438aba5e9342eb0f810f4e
Grzegorz Maniak <git@grzegorz.ie>
2023-06-25 17:03:31 +0100

    Seperated files

--- src/anchor.ts
+++ src/anchor.ts
@@ -1,4 +1,6 @@
-import { Line } from './index.d';
+import { render_line } from './render';
+import { get_client_size } from './aux';
+import { Anchor, Line } from './index.d';
 
 /**
  * @name init_anchors
@@ -73,4 +75,88 @@ export const add_anchor = (line: Line, x?: number, y?: number) => {
         y: anchor.max_y - handle.height() / 2 + line.handle_padding,
     });
     anchor.handle_y = handle.position().y;
+};
+
+
+
+/**
+ * @name handle_listener
+ * @description Handles the listener for the handle of an anchor
+ * allowing it to move left and right
+ * @param {Line} line The line
+ * @param {Anchor} anchor The anchor
+ * @returns {void} Nothing
+ */
+export const handle_listener = (line: Line, anchor: Anchor) => {
+    anchor.handle.on('dragmove', () => {
+        // -- Get the handle position
+        const [width, height] = get_client_size(),
+            { x, y } = anchor.handle.position();
+
+        const const_y = anchor.handle_y,
+            min_x = 0,
+            max_x = width;
+
+        const new_x = Math.max(min_x, Math.min(max_x, x));
+
+        // -- Set the handle position
+        anchor.handle.position({
+            x: new_x,
+            y: const_y,
+        });
+        
+        // -- Move the anchor
+        anchor.shape.position({
+            x: new_x - (anchor.shape.width() / 2) + (anchor.handle.width() / 2),
+            y: anchor.shape.position().y,
+        });
+
+        // -- Render the line
+        render_line(line);
+    });
+};
+
+
+
+/**
+ * @name anchor_listener
+ * @description Handles the listener for the anchor of a line
+ * allowing it to move up and down
+ * @param {Line} line The line
+ * @param {Anchor} anchor The anchor
+ * @returns {void} Nothing
+ */
+export const anchor_listener = (line: Line, anchor: Anchor) => {
+    anchor.shape.on('dragmove', () => {
+        // -- Get the anchor position
+        const { x, y } = anchor.shape.position();
+
+        // -- Make sure that the anchor is within the line
+        const max_y = anchor.max_y,
+            min_y = anchor.min_y;
+
+        // -- Get the cnst X from the handle
+        const handle_pos = anchor.handle.position(),
+            handle_size = anchor.handle.size(),
+            const_x = handle_pos.x + handle_size.width / 2;
+
+        // -- Set the anchor position
+        const width = anchor.shape.width(),
+            height = anchor.shape.height();
+
+        // -- Set the anchor position
+        anchor.shape.position({
+            x: const_x - width / 2,
+            y: Math.max(
+                min_y - height / 2, 
+                Math.min(max_y - height / 2, y)
+            ),
+        });
+
+        // -- Render the line
+        render_line(line);
+    });
+
+    // -- Render the line
+    render_line(line);
 };
---
+++ src/render.ts
@@ -0,0 +1,145 @@
+import { Line } from './index.d';
+
+
+/**
+ * @name sort_anchors
+ * @description Sorts the anchors by x position
+ * used for rendering the line correctly
+ * @param {Line} line The line
+ * @returns {void} Nothing
+ */
+export const sort_anchors = (line: Line) => {
+    line.anchors.sort((a, b) => {
+        const a_pos = a.shape.position(),
+            b_pos = b.shape.position();
+        if (a_pos.x < b_pos.x) return -1;
+        if (a_pos.x > b_pos.x) return 1;
+        return 0;
+    });
+}
+
+
+
+const render_line_points = (line: Line): void => {
+    let points: Array<number> = [];
+
+    // -- Main lines
+    line.anchors.forEach((anchor) => {
+        const { x, y } = anchor.shape.position();
+        points.push(
+            x + anchor.shape.width() / 2,
+            y + anchor.shape.height() / 2,
+        );
+    });
+
+
+    // -- First and last Anchors, this is done to close
+    //    off the line at the correct height.
+    (() => {
+        // -- Last Anchor
+        const anchor = line.anchors[line.anchors.length - 1],
+        { x, y } = anchor.shape.position();
+
+        // -- Last Anchor
+        points = [
+            ...points,
+            x + anchor.shape.width() / 2,
+            anchor.min_y + line.height,
+        ];
+    })();
+
+    (() => {
+        // -- First Anchor
+        const anchor = line.anchors[0],
+        { x, y } = anchor.shape.position();
+
+        // -- First Anchor
+        points = [
+            ...points,
+            x + anchor.shape.width() / 2,
+            anchor.min_y + line.height,
+        ];
+    })();
+
+
+    // -- Set the points
+    line.line.points(points);
+}
+
+
+
+const render_depth_line = (line: Line) => {
+    
+    let points: Array<number> = [];
+    line.anchors.forEach((anchor) => {
+        const { x, y } = anchor.shape.position();
+        points.push(
+            x + anchor.shape.width() / 2,
+            anchor.max_y,
+        );
+    });
+
+    // -- First and last Anchors, sets nice buffers
+    //    on either side of the line.
+    (() => {
+        const anchor = line.anchors[line.anchors.length - 1],
+        { x, y } = anchor.shape.position();
+
+        // -- Last Anchor
+        points = [
+            ...points,
+            (x + anchor.shape.width() / 2) + line.depth_buffer,
+            anchor.max_y,
+        ];
+    })();
+
+    (() => {
+        const anchor = line.anchors[0],
+        { x, y } = anchor.shape.position();
+
+        // -- First Anchor
+        points = [
+            (x + anchor.shape.width() / 2) - line.depth_buffer,
+            anchor.max_y,
+            ...points,
+        ];
+    })();
+
+    // -- Set the points
+    line.depth_line.points(points);
+};
+
+
+
+const render_anchor_guides = (line: Line) => {
+    line.anchors.forEach((anchor) => {
+        
+        // -- Get the handle position
+        const h_pos = anchor.handle.position(),
+            h_size = anchor.handle.size(),
+            x = h_pos.x + h_size.width / 2;
+
+        // -- Set the line position
+        anchor.line.points([
+            x, anchor.min_y,
+            x, anchor.max_y + line.handle_padding,
+        ]);
+    });
+};
+
+
+
+/**
+ * @name render_line
+ * @description Renders everything to do with
+ * the line, including the anchors, the depth
+ * line, anchor guides etc.
+ * @param {Line} line The line to render
+ */
+export const render_line = (line: Line) => {
+    sort_anchors(line);
+    render_line_points(line);
+    render_depth_line(line);
+    render_anchor_guides(line);
+    line.get_layer().draw();
+};
